11543d8693778a1b06e44f7d6cbe306ab3a4f9af
JayoonKoo <rnwkdbs12@gmail.com>
2022-02-22 15:49:27 +0900

    feat: certification hook 추가

--- src/hooks/SignUp/useCertification.ts
+++ src/hooks/SignUp/useCertification.ts
@@ -1,3 +1,28 @@
-const useCertification = () => {};
+import AuthService from 'api/Auth/AuthService';
+import {useNavigate} from 'react-router-dom';
+
+const useCertification = () => {
+  const {IMP} = window as any;
+  const navigate = useNavigate();
+  IMP.init('imp15289551');
+  const certification = () => {
+    IMP.certification({}, (rsp: any) => {
+      if (rsp.success) {
+        AuthService.certification(rsp.imp_uid)
+          .then(() => {
+            navigate('/signup');
+          })
+          .catch(error => {
+            throw error;
+          });
+      } else {
+        console.dir(rsp);
+        alert(rsp.error_msg);
+      }
+    });
+  };
+
+  return certification;
+};
 
 export default useCertification;
