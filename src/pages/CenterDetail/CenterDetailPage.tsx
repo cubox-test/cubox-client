da3e8e9c666fabd588144bea0b734d9e53a91c35
JayoonKoo <rnwkdbs12@gmail.com>
2022-02-28 16:54:27 +0900

    feat: cneterDetail 페이지 생성

--- src/pages/CenterDetail/CenterDetailPage.tsx
+++ src/pages/CenterDetail/CenterDetailPage.tsx
@@ -1,9 +1,57 @@
-// import {useParams} from 'react-router-dom';
+import CenterService from 'api/Center/CenterService';
+import {GetJobsInfoByCenterIdRes} from 'api/Center/centerType';
+import useFetch from 'hooks/Common/useFetch';
+import {useParams} from 'react-router-dom';
+import styled from 'styled-components';
+import AssignedSection from './components/AssignedSection';
+import TotalSection from './components/TotalSection';
+import WaitingSection from './components/WaitingSection';
 
 function CenterDetailPage() {
-  // const {centerId} = useParams<{centerId: string}>();
+  const {centerId} = useParams<{centerId: string}>();
+  const {data: jobs} = useFetch(
+    {centerId: centerId!},
+    CenterService.getJobsInfoByCenterId,
+  );
 
-  return <div>CenterDetailPage</div>;
+  const {assignedJobs, waitingJobs} = classifyJobs(jobs);
+
+  return (
+    <>
+      {jobs && (
+        <Wrapper>
+          <TotalSection jobs={jobs} />
+          <AssignedSection jobs={assignedJobs!} />
+          <WaitingSection jobs={waitingJobs!} />
+        </Wrapper>
+      )}
+    </>
+  );
+}
+
+const Wrapper = styled.div``;
+
+function isWaiting(assingState: number) {
+  return assingState === 1;
+}
+
+function classifyJobs(jobs: GetJobsInfoByCenterIdRes | null) {
+  if (jobs == null) {
+    return {assignedJobs: null, waitingJobs: null};
+  }
+  const assignedJobs: GetJobsInfoByCenterIdRes = [];
+  const waitingJobs: GetJobsInfoByCenterIdRes = [];
+
+  jobs.forEach(job => {
+    const {assignState} = job;
+    if (isWaiting(assignState)) {
+      waitingJobs.push(job);
+    } else {
+      assignedJobs.push(job);
+    }
+  });
+
+  return {assignedJobs, waitingJobs};
 }
 
 export default CenterDetailPage;
