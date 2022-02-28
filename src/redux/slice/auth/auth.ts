de4ba5296299bdde02a328a91f6a8acd21396926
JayoonKoo <rnwkdbs12@gmail.com>
2022-02-28 14:43:14 +0900

    feat: roleId, userId 전역상태로 추가

--- src/redux/slice/auth/auth.ts
+++ src/redux/slice/auth/auth.ts
@@ -1,18 +1,33 @@
 import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
-const initialState = {
+type InitialState = {
+  isAuth: boolean;
+  userId: string | null;
+  roleId: number | null;
+};
+
+const initialState: InitialState = {
   isAuth: false,
+  userId: null,
+  roleId: null,
 };
 
 const authSlice = createSlice({
   name: 'auth',
   initialState,
   reducers: {
-    authenticate(state, action: PayloadAction<{isAuth: boolean}>) {
+    authenticate(
+      state,
+      action: PayloadAction<{isAuth: boolean; roleId: number | null}>,
+    ) {
       state.isAuth = action.payload.isAuth;
+      state.roleId = action.payload.roleId;
+    },
+    setUserId(state, action: PayloadAction<{userId: string | null}>) {
+      state.userId = action.payload.userId;
     },
   },
 });
 
 export default authSlice.reducer;
-export const {authenticate} = authSlice.actions;
+export const {authenticate, setUserId} = authSlice.actions;
